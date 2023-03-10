2ac264b4005f9aa561ccd6185f31fc3e47fd23cb
marikadiblasio <marbla.albram@gmail.com>
2023-03-10 15:03:31 +0100

    scaffolding

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* *Consegna:*
+ Scrivi un programma che stampi in console i numeri da 1 a 100,
+ ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
+ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*Consigli del giorno:*
+1. Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"*/
+
+//prova a stampare - pensa all'if - stilizza
+//console.log
+for ( let i = 1; i <= 100; i++ ){
+    console.log(i);
+}
